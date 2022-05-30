b5c4dc479950909389337191e753e5dd0e359073
José Rodolpho <j.rodolpho29@hotmail.com>
2022-05-30 16:50:31 -0300

    feat: heroku connection

--- database.js
+++ database.js
@@ -4,12 +4,8 @@ dotenv.config()
 
 let chachedDB = null;
 let connectionParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-};
+ 
+}
 
 export default async function connectDB() {
   if (chachedDB) {
@@ -19,7 +15,10 @@ export default async function connectDB() {
   if (process.env.DB_URL) {
     connectionParams = {
       connectionString: process.env.DB_URL,
-    };
+      ssl: {
+          rejectUnauthorized: false
+      }
+    }
   }
 
   const { Pool } = pg
--- index.js
+++ index.js
@@ -15,4 +15,6 @@ app.use(clientRouter)
 app.use(categoryRouter)
 app.use(gameRouter)
 
-app.listen(4000)
+app.listen(process.env.DB_PORT, () => {
+    console.log("Server running on port " + process.env.DB_PORT)
+})
